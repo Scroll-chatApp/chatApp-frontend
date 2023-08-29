85a10e9198590c044771b1202058b74426617c6b
Athar Ateeq <atharateeq01@gmail.com>
2023-08-29 18:46:21 +0500

    Fix project directory

--- src/component/userJoin/Join.js
+++ src/component/userJoin/Join.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import scrollLogo from "../../image/scrollLogo.png";
+// import axios from "axios";
 import "./join.css";
 
 let user;
