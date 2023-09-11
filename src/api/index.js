d78ca2d0cac974d87e93ef35cfa8446f678abdf7
athar-shayansolutions <athar.shayansolutions@gmail.com>
2023-09-11 18:45:26 +0500

    Made UI changes - Update new message display - made responsive - tried PDF properl

--- src/api/index.js
+++ src/api/index.js
@@ -3,12 +3,16 @@ import { ENDPOINT } from "../constant";
 
 const API = axios.create({ baseURL: ENDPOINT });
 
-export const conversationIdFetch = async (senderId,receiverId ) => {
-  const conversation = await API.get(
-    `/getallconversationofuser/${senderId}/${receiverId}`
-  );
-  const fetchedConversation = conversation.data;
-  return fetchedConversation;
+export const conversationIdFetch = async (senderId, receiverId) => {
+  try {
+    const conversation = await API.get(
+      `/getallconversationofuser/${senderId}/${receiverId}`
+    );
+    const fetchedConversation = conversation.data;
+    return fetchedConversation;
+  } catch (error) {
+    console.error("Error fetching conversation:", error);
+  }
 };
 
 export const fetchAllMessages = async  (conversationId) => {
@@ -17,16 +21,15 @@ export const fetchAllMessages = async  (conversationId) => {
   return fetchedMessage;
 };
 
-export const createNewConversation = async  (senderId,receiverId) => {
-    const createConversation = await API.post(
-        `/newconversation/${senderId}/${receiverId}`
-      );
-    return createConversation;
-  };
-  
+export const createNewConversation = async  (senderId, receiverId) => {
+  const createConversation = await API.post(
+    `/newconversation/${senderId}/${receiverId}`
+  );
+  return createConversation;
+};
+
 export const fetchAllUser = async  () => {
-    const response = await API.get(`/getallusers`);
-    const fetchedUsers = response.data;
-    return fetchedUsers;
-  };
-  
+  const response = await API.get(`/getallusers`);
+  const fetchedUsers = response.data;
+  return fetchedUsers;
+};
--- src/component/Chat/ChatArea/pdfView.js
+++ src/component/Chat/ChatArea/pdfView.js
@@ -5,7 +5,7 @@ export default function DownloadPDF({ pdfUrl }) {
   return (
     <div>
       <p>Click the button below to download the PDF:</p>
-      <a href={pdfUrl}>Download PDF</a>
+      <a download href={pdfUrl}>Download PDF</a>
     </div>
   );
 }
